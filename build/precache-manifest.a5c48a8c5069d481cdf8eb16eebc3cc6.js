244aa611ae257095d514a475119b011611fecd81
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2019-02-25 09:47:09 +0000

    Node modules addition

---
+++ build/precache-manifest.a5c48a8c5069d481cdf8eb16eebc3cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "84506148cdf5206ed4d0",
+    "url": "/static/js/main.84506148.chunk.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "84506148cdf5206ed4d0",
+    "url": "/static/css/main.d800cfa4.chunk.css"
+  },
+  {
+    "revision": "5c25c49d1575b06085a2c24932af18f9",
+    "url": "/index.html"
+  }
+];
